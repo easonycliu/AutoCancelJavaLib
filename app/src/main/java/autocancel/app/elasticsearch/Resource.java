20eafccb7922d806569404d7d38bdb61d0288240
liuyc <liuyicheng1515@sjtu.edu.cn>
2023-08-22 20:50:33 +0800

    add app resource monitor api

--- app/src/main/java/autocancel/app/elasticsearch/Resource.java
+++ app/src/main/java/autocancel/app/elasticsearch/Resource.java
@@ -2,4 +2,15 @@ package autocancel.app.elasticsearch;
 
 public class Resource {
     
+    public void addResourceUsage(String name, Double value) {
+        
+    }
+
+    public void addResourceOccupy(String name) {
+
+    }
+
+    public void addResourceWait(String name) {
+
+    }
 }
