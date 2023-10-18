de9692a268ab9562467dd1a6874d5d522d8547ea
liuyc <liuyicheng1515@sjtu.edu.cn>
2023-10-18 17:18:32 +0800

    finish MOOPolicy

--- app/src/main/java/autocancel/core/policy/MOOPolicy.java
+++ app/src/main/java/autocancel/core/policy/MOOPolicy.java
@@ -1,5 +1,6 @@
 package autocancel.core.policy;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import autocancel.utils.Policy;
@@ -22,15 +23,39 @@ public class MOOPolicy extends Policy {
 
     @Override
     public CancellableID getCancelTarget() {
-        
-        return null;
+        Map<CancellableID, Map<ResourceName, Long>> cancellableGroupUsage = this.infoCenter.getCancellableGroupUsage();
+        Map<ResourceName, Double> weight = this.infoCenter.getContentionLevel();
+        Map<CancellableID, Double> weightedSum = new HashMap<CancellableID, Double>();
+        for (Map.Entry<CancellableID, Map<ResourceName, Long>> cancellableGroupUsageEntry : cancellableGroupUsage.entrySet()) {
+            weightedSum.put(cancellableGroupUsageEntry.getKey(), MOOPolicy.calculateWeightedSum(weight, cancellableGroupUsageEntry.getValue()));
+        }
+        Map.Entry<CancellableID, Double> maxWeightedSum = weightedSum
+                                                            .entrySet()
+                                                            .stream()
+                                                            .max(Map.Entry.comparingByValue())
+                                                            .orElse(null);
+        CancellableID target = null;
+        if (maxWeightedSum != null) {
+            target = maxWeightedSum.getKey();
+        }
+
+        if (target == null) {
+            System.out.println("Failed to find a target to cancel for unknown reason");
+            target = new CancellableID();
+        }
+        else if (!this.infoCenter.isCancellable(target)) {
+            System.out.println(target.toString() + " is not cancellable");
+            target = new CancellableID();
+        }
+
+        return target;
     }
 
-    static private Double calculateWeightedSum(Map<ResourceName, Double> weight, Map<ResourceName, Double> resourceUsages) {
+    static private Double calculateWeightedSum(Map<ResourceName, Double> weight, Map<ResourceName, Long> resourceUsages) {
         Double sum = 0.0;
-        for (Map.Entry<ResourceName, Double> usageEntry : resourceUsages.entrySet()) {
+        for (Map.Entry<ResourceName, Long> usageEntry : resourceUsages.entrySet()) {
             try {
-                sum += Math.max(usageEntry.getValue() * weight.get(usageEntry.getKey()), 0.0);
+                sum += Math.max(Double.valueOf(usageEntry.getValue()) * weight.get(usageEntry.getKey()), 0.0);
             }
             catch (NullPointerException e) {
                 throw new AssertionError(String.format("%s name is not in weight map", usageEntry.getKey()));
