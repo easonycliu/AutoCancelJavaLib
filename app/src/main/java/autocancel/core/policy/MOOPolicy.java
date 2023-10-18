de952e06e349abb3a6dc1601d14be838e3e0a963
liuyc <liuyicheng1515@sjtu.edu.cn>
2023-10-18 17:06:18 +0800

    MOO policy framework

--- app/src/main/java/autocancel/core/policy/MOOPolicy.java
+++ app/src/main/java/autocancel/core/policy/MOOPolicy.java
@@ -1,7 +1,10 @@
 package autocancel.core.policy;
 
+import java.util.Map;
+
 import autocancel.utils.Policy;
 import autocancel.utils.id.CancellableID;
+import autocancel.utils.resource.ResourceName;
 
 public class MOOPolicy extends Policy {
     
@@ -19,6 +22,20 @@ public class MOOPolicy extends Policy {
 
     @Override
     public CancellableID getCancelTarget() {
+        
         return null;
+    }
+
+    static private Double calculateWeightedSum(Map<ResourceName, Double> weight, Map<ResourceName, Double> resourceUsages) {
+        Double sum = 0.0;
+        for (Map.Entry<ResourceName, Double> usageEntry : resourceUsages.entrySet()) {
+            try {
+                sum += Math.max(usageEntry.getValue() * weight.get(usageEntry.getKey()), 0.0);
+            }
+            catch (NullPointerException e) {
+                throw new AssertionError(String.format("%s name is not in weight map", usageEntry.getKey()));
+            }
+        }
+        return sum;
     }
 }
