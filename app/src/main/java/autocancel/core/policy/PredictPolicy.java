71dab2edb7974dc40d17b8a9db1da3acb4888917
liuyc <liuyicheng1515@sjtu.edu.cn>
2023-10-18 21:16:43 +0800

    finish predict policy prototype

---
+++ app/src/main/java/autocancel/core/policy/PredictPolicy.java
@@ -0,0 +1,23 @@
+package autocancel.core.policy;
+
+import autocancel.utils.Policy;
+import autocancel.utils.id.CancellableID;
+
+public class PredictPolicy extends Policy {
+    private CancelTrigger trigger;
+
+    public PredictPolicy() {
+        super();
+        this.trigger = new CancelTrigger();
+    }
+
+    @Override
+    public Boolean needCancellation() {
+        return this.trigger.triggered(this.infoCenter.getFinishedTaskNumber());
+    }
+
+    @Override
+    public CancellableID getCancelTarget() {
+        return null;
+    }
+}
